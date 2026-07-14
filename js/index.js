253e235f122cbc1bf01ecfa9ff2c9bd751530d0c
masterstudiosv <masterstudiosv@gmail.com>
2026-07-14 07:33:09 -0600

    Update index.js

--- js/index.js
+++ js/index.js
@@ -1,19 +1,11 @@
 // Cargar logos automáticamente desde la carpeta clientes/
         // Lista todos los archivos SVG que tengas en la carpeta clientes/
-        fetch('obtener-logos.php')
-    .then(response => response.json())
-    .then(logoFiles => {
-
-        const logoTrack = document.getElementById('logoTrack');
-
-        logoFiles.forEach(file => {
-            const img = document.createElement('img');
-            img.src = `clientes/${file}`;
-            img.alt = file;
-            img.className = 'logo-item';
-
-            logoTrack.appendChild(img);
-        });
+        const logoFiles = [
+            'cli1.svg', 'cli2.svg', 'cli3.svg', 'cli4.svg', 
+            'cli5.svg', 'cli6.svg', 'cli7.svg', 'cli8'
+            // Agrega aquí cualquier logo nuevo que subas a clientes/
+            // Ejemplo: 'cli8.svg', 'cli9.svg', 'cli10.svg'
+        ];
 
         // Si duplicas los logos para hacer un carrusel infinito
         logoFiles.forEach(file => {
